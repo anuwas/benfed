f970e8b15fbc4d4841cc0d15d17e44b536d64872
ANUWAS <info.anuwas@gmail.com>
2017-04-14 18:46:20 +0530

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=benfed',
-    'username' => 'root',
-    'password' => 'root',
+    'dsn' => 'mysql:host=localhost;dbname=dbname',
+    'username' => 'username',
+    'password' => 'password',
     'charset' => 'utf8',
 ];
